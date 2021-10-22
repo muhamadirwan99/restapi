a4037ff48b5441e0b50b6ca31e69d1b486e1a80b
Muhamad Irwan <muhamadirwan99@gmail.com>
2021-10-22 07:23:10 +0700

    Add multer, setting multer, addfile on controller.js, add route

--- controller.js
+++ controller.js
@@ -6,3 +6,21 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API Berjalan!", res);
 }
+
+//add file
+exports.addFile = function(req, res){
+    var nama = req.body.nama;
+    var tanggal = req.body.tanggal;
+    var image = req.file.path;
+
+    connection.query('INSERT INTO ulangtahun (nama, tanggal, image) VALUES(?,?,?)',
+        [nama, tanggal, image],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil menambahkan data!", res);
+            }
+        }
+    );
+};
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const multer = require('multer');
 const app= express();
 
 //parse application/jsonbody
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+//setup multer
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toString() + '-' + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg'){
+        cb(null, true);
+    } else{
+        cb(null, false);
+    }
+}
+
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+
 //panggil routes
 var routes = require('./routes');
 routes(app);
 
 app.listen(3000, () => {
-    console.log(`Server started on port`);
+    console.log(`Server started on port 3000`);
 });
